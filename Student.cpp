25d32ffe01c56eae857f643448f8a984c3813ec8
haley <huanghongweimax@163.com>
2024-06-26 15:30:28 +0800

    实现学生类

---
+++ Student.cpp
@@ -0,0 +1,30 @@
+#include "Student.h"
+
+Student::Student() : m_Name("Empty") , m_Age(0){
+    std::cout << "学生类创建" << std::endl;
+}
+
+Student::~Student(){
+    std::cout << "学生类被销毁" << std::endl;
+}
+
+string Student::get_name(){
+    return this->m_Name;
+}
+
+void Student::set_name(string name){
+    this->m_Name = name;
+}
+
+ unsigned Student::get_age(){
+    return this->m_Age;
+ }
+
+void Student::set_age(unsigned age){
+    this->m_Age = age;
+}
+
+void Student::print(){
+    std::cout << "name : " << m_Name << "age : " << m_Age << std::endl;
+}
+
