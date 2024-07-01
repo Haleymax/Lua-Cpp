aad7e2d1b5fde4bd4e6cabc4e11ecc3d1db5a46a
haley <huanghongweimax@163.com>
2024-07-01 11:58:19 +0800

    创建MYSQL的操作

---
+++ test2-Sql/mysql_lua.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include <mysql/mysql.h>
+
+extern "C"
+{
+#include "../lua-5.4.6/src/lua.h"
+#include "../lua-5.4.6/src/lauxlib.h"
+#include "../lua-5.4.6/src/lualib.h"
+}
+
+//打开MySQL Lua库的函数声明
