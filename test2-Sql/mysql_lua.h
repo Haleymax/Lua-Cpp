b35255d3998e2740372d8171ed80f0c14eb5dc02
haley <huanghongweimax@163.com>
2024-07-01 13:38:33 +0800

    声明操作mysql的函数“

--- test2-Sql/mysql_lua.h
+++ test2-Sql/mysql_lua.h
@@ -11,3 +11,11 @@ extern "C"
 }
 
 //打开MySQL Lua库的函数声明
+int luaopen_mysql_lib(lua_State *L);
+
+//MySQL 连接和操作函数
+int lua_mysql_connect(lua_State *L);    //连接
+int lua_mysql_insert(lua_State *L);     //插入
+int lua_mysql_select(lua_State *L);     //查询
+int lua_mysql_update(lua_State *L);     //修改
+int lua_mysql_delete(lua_State *L);     //删除
